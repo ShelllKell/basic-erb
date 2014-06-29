7fd8c5253df984d1c2892a4c0cc2031e05b48df5
Shelby Kelly <shelby.kelly@colorado.edu>
2014-06-29 14:51:01 -0600

    Final working version of html generator

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -2,15 +2,22 @@ require "erb_generator"
 
 class HTMLGenerator < ERBGenerator
 
-  def section(text)
-      template = "<section><%= some_text%></section>"
-      options_hash = {:locals => {:some_text => text}}
+  def initialize(layout = nil)
+    @layout = layout
+  end
+
+
+  def section(string)
+   template = "<section><%=some_text%></section>"
+
+      options_hash = {:locals => {:some_text => string}, :layout => @layout}
       erb template, options_hash
 
   end
 
   def unordered_list(items)
       template = <<-TEMPLATE
+
 
   <ul>
 
@@ -22,7 +29,7 @@ class HTMLGenerator < ERBGenerator
 
       TEMPLATE
 
-    options_hash = {:locals => {:html_items => items}}
+    options_hash = {:locals => {:html_items => items}, :layout => @layout}
     erb(template, options_hash)
 
   end
@@ -31,10 +38,10 @@ class HTMLGenerator < ERBGenerator
 
     if options == nil
       x = "<button><%=button_text%></button>"
-      y = {:locals => {:button_text => text}}
+      y = {:locals => {:button_text => text}, :layout => @layout}
     else
       x = "<button class='<%=button_class%>'><%=button_text%></button>"
-      y = {:locals => {:button_text => text, :button_class => options[:class]}}
+      y = {:locals => {:button_text => text, :button_class => options[:class]}, :layout => @layout}
     end
 
     template = x
@@ -42,5 +49,7 @@ class HTMLGenerator < ERBGenerator
     erb template, options_hash
 
   end
+
+
 
 end
